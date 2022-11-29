ae9a89e94e0407264de4511be98e54b95e409c7c
Willie-The-Lord <willie880201044@gmail.com>
2022-11-29 13:47:27 -0600

    Upload Samurai project

---
+++ tile.py
@@ -0,0 +1,20 @@
+import pygame
+from settings import *
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, pos, groups, sprite_type, surface = pygame.Surface((TILESIZE, TILESIZE))):
+        super().__init__(groups)
+        self.sprite_type = sprite_type
+        y_offset = HITBOX_OFFSET[sprite_type]
+        self.image = surface
+        # change larger objects 
+        if sprite_type == 'object':
+            # do an offset
+            self.rect = self.image.get_rect(topleft = (pos[0], pos[1] - TILESIZE))
+        else:
+            self.rect = self.image.get_rect(topleft = pos)
+        # create hitbox, hitbox should be smaller
+        self.hitbox = self.rect.inflate(0, y_offset)
+
+    
+    
